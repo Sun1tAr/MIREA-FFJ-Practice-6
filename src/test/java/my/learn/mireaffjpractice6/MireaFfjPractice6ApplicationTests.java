b4c81f0c54f7c43956459e266ab5c0f20b120c7d
Sun1tAr <denchik2376@yandex.ru>
2025-11-07 18:33:30 +0300

    Initial commit

---
+++ src/test/java/my/learn/mireaffjpractice6/MireaFfjPractice6ApplicationTests.java
@@ -0,0 +1,13 @@
+package my.learn.mireaffjpractice6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MireaFfjPractice6ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
